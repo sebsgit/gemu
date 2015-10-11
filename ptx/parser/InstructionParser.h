ae988279968fdfb9698f514d3f8f1e9d71bd3c46
sebsgit <s.baginski@mail.com>
2015-10-11 11:26:26 +0200

    create parsers once in instruction parser

--- ptx/parser/InstructionParser.h
+++ ptx/parser/InstructionParser.h
@@ -22,10 +22,10 @@
 namespace ptx {
 	namespace parser {
 		class InstructionParser : public AbstractParser {
-		protected:
-			bool parseTokens(TokenList& tokens, ParserResult& result) const override {
-				bool parsedOk = false;
-				std::vector<std::shared_ptr<AbstractParser>> parsers;
+		private:
+			std::vector<std::shared_ptr<AbstractParser>> parsers;
+		public:
+			InstructionParser(){
 				parsers.push_back(std::make_shared<VariableParser>());
 				parsers.push_back(std::make_shared<StoreParser>());
 				parsers.push_back(std::make_shared<LoadParser>());
@@ -35,19 +35,23 @@ namespace ptx {
 				parsers.push_back(std::make_shared<BranchParser>());
 				parsers.push_back(std::make_shared<SetpParser>());
 				parsers.push_back(std::make_shared<MulParser>());
-                parsers.push_back(std::make_shared<DivParser>());
+				parsers.push_back(std::make_shared<DivParser>());
 				parsers.push_back(std::make_shared<AddParser>());
 				parsers.push_back(std::make_shared<AndParser>());
 				parsers.push_back(std::make_shared<MadParser>());
-                parsers.push_back(std::make_shared<SubParser>());
+				parsers.push_back(std::make_shared<SubParser>());
 				parsers.push_back(std::make_shared<ShlParser>());
 				parsers.push_back(std::make_shared<BarrierParser>());
+			}
+		protected:
+			bool parseTokens(TokenList& tokens, ParserResult& result) const override {
+				bool parsedOk = false;
 				std::string label;
 				std::string predicate;
 				bool predicateNegated=false;
 				Utils::parseLabel(tokens, &label);
 				Utils::parsePredicate(tokens, &predicate, &predicateNegated);
-				for (const auto& p : parsers) {
+				for (const auto& p : this->parsers) {
 					if (p->parse(tokens, result)) {
 						if (label.empty()==false)
 							result.labelLast(label);
