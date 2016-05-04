3898ff586d7efca5a6474266d6c31a2b961bcbc0
sebsgit <s.baginski@mail.com>
2016-05-04 20:54:01 +0200

    added left shift

--- ptx/semantics/instructions/int/Shl.h
+++ ptx/semantics/instructions/int/Shl.h
@@ -11,9 +11,32 @@ namespace ptx {
 		std::string toString() const override {
 			return "<shl> " + MemoryInstruction::toString();
 		}
-//        void resolve(SymbolTable& symbols) const override {
+		void resolve(SymbolTable& symbols) const override {
+			param_storage_t dest;
+			const auto src = symbols.get(this->_operands[1]);
+			const int shiftby = param_cast<unsigned>(symbols.get(this->_operands[2]));
+			switch(this->type()) {
+			case Type::Bits:
+				if (this->size() < 8)
+					this->do_shift<unsigned int>(dest, src, shiftby);
+				else
+					this->do_shift<unsigned long long>(dest, src, shiftby);
+				break;
+			default:
+				//TODO error
+				break;
+			}
 
-//        }
+			symbols.set(this->_operands[0], dest);
+		}
+	private:
+		template <typename T>
+		void do_shift(param_storage_t& dest, const param_storage_t& src, int shiftby) const noexcept {
+			if (shiftby >= sizeof(T) * 8)
+				param_cast<T>(dest) = param_cast<T>(src) > 0 ? (T)0 : (T)~0;
+			else
+				param_cast<T>(dest) = param_cast<T>(src) << shiftby;
+		}
 	};
 
     class Shr : public MemoryInstruction {
